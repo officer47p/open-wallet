b75a0990eaefa5824f826e5e3c0376bf2f39a06d
Parsa Hosseini <officer47p@gmail.com>
2022-06-23 11:03:19 +0430

    Refactor key manager get private key and add support for un-compressed public key

--- KeyManagment/__init__.py
+++ KeyManagment/__init__.py
@@ -1,5 +1,9 @@
 from mnemonic import Mnemonic
 from bip32 import BIP32
+import ecdsa
+from ecdsa.keys import VerifyingKey
+from ecdsa.curves import SECP256k1
+from hashlib import sha256
 
 
 class KeyManager(object):
@@ -12,7 +16,7 @@ class KeyManager(object):
         seed = mnemo.to_seed(words, passphrase="")
         return cls(seed)
 
-    def get_private_key_from_path(self, path, format='bytes'):
+    def get_private_key(self, path='m', format='bytes'):
         bip32 = BIP32.from_seed(self.seed)
         private_key = bip32.get_privkey_from_path(path)
 
@@ -21,9 +25,12 @@ class KeyManager(object):
         elif format == 'hex':
             return private_key.hex()
 
-    def get_public_key_from_path(self, path, format='bytes'):
+    def get_public_key(self, path='m', format='bytes', compressed=True):
         bip32 = BIP32.from_seed(self.seed)
         public_key = bip32.get_pubkey_from_path(path)
+        if not compressed:
+            vk = VerifyingKey.from_string(public_key, curve=SECP256k1)
+            public_key = vk.to_string('uncompressed')
 
         if format == 'bytes':
             return public_key
--- main.py
+++ main.py
@@ -4,8 +4,14 @@ from networks.bitcoin import BitcoinWallet
 manager = KeyManager.from_mnemonic(
     'race romance science affair sad just seminar lens diary relief glimpse horror')
 
-key = manager.get_private_key_from_path("m/1/1", 'hex')
-print(f'Key: {key}')
+private_key = manager.get_private_key(path="m/1/1")
+public_key = manager.get_public_key(path="m/1/1", compressed=False)
 
-address = BitcoinWallet.get_address(key, testnet=True)
+print(f'Private Key: {private_key.hex()}')
+print(f'Public Key: {public_key.hex()}')
+
+address = BitcoinWallet.get_address(private_key)
 print(f'Address: {address}')
+
+signature = KeyManager.sign(private_key, b"Hello there")
+print(f'Signature: {signature.hex()}')
